ca17d13a390a1973821276aec5aeffb874b5bd47
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-09-11 11:37:28 -0400

    Trello roadmap uses ajax #96
    The trello roadmap now uses ajax to fetch statuses from github.  I'm still using my big queries instead of looping over tickets, but this should speed up page loads even more.
    
    I also cached the statuses.  When the ajax call builds its array of status => [list,of,ids], it caches the whole thing for 30 minutes.  When the roadmap table is built, if the cached data is valid it gets provided to Drupal.settings.  When that setting is present and nonempty, javascript will use it instead of ajax.

---
+++ trello_integration/trello_integration_roadmap.js
@@ -0,0 +1,35 @@
+/**
+ * Loads trello roadmap statuses via ajax
+ **/
+
+(function ($) {
+
+Drupal.behaviors.trello_integration_roadmap = { 
+  attach: function (context) {
+    settings = Drupal.settings.trello_integration_roadmap;
+    
+    //loop over known statuses and their gh ids, applying statuses when foudn.
+    var display_statuses = function(data) {
+      for (var status in data) {
+        for (var id in data[status]) {
+          $('.roadmap span.get-status.status-'+data[status][id])
+            .addClass(status.toLowerCase())
+            .removeClass('get-status')
+            .html(status)
+        }
+      }
+      
+      $('.roadmap .get-status').removeClass('get-status'); //clear remaining throbbers
+    };
+    
+    //use preloaded statuses or fetch and then display them
+    if (settings.roadmap_statuses) {
+      display_statuses(settings.roadmap_statuses);
+    } else {
+      //add throbber
+      $.getJSON(settings.ajax_path, '', display_statuses);
+    }
+  }
+};
+
+})(jQuery);  
