05332c853fddd4f25a7db9a27e387687d7211f26
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2013-09-16 15:43:26 -0400

    Changed to three column layout #96
    Changing the markup coming out wasn't so bad, but updating css and js got obnoxious.
    
    Had to update my status fetching ajax calls a bit.  What I was doing was a fetch per status bucket.  ie fetch all 'status: needs qa' and all 'status: fixed'.  Store all the github ids that had that label and display that status.
    
    Two problems.  One, I was assuming every ticket would get 'status: fixed' and that any that didn't were dev errors.  That wasn't a helpful assumption.  Two was worse.  I forgot that each search query only gives me open tickets.  I'd need a second one to get all closed tickets for that status.
    
    Getting all those closed tickets was extremely slow.  Slow enough that it wasn't reliable that I'd have a network connection the whole time.  Instead of getting them all at once or even per status, I queried for them per milestone.  Seems speedy and it will give us exactly one extra api call per milestone page.

--- trello_integration/trello_integration_roadmap.js
+++ trello_integration/trello_integration_roadmap.js
@@ -7,27 +7,56 @@
 Drupal.behaviors.trello_integration_roadmap = { 
   attach: function (context) {
     settings = Drupal.settings.trello_integration_roadmap;
+    console.log(settings)
     
     //loop over known statuses and their gh ids, applying statuses when foudn.
     var display_statuses = function(data) {
+      var id_status = {};
       for (var status in data) {
-        for (var id in data[status]) {
-          $('.roadmap span.get-status.status-gh-'+data[status][id])
-            .addClass(status.toLowerCase())
-            .removeClass('get-status')
-            .html(status)
+        for (var i in data[status]) {
+          id_status[ data[status][i] ] = status;
         }
       }
+      console.log(id_status)
+      $('a.get-status').each(function() {
+        var $this = $(this);
+        var this_id = $this.attr('id').split('-')[2];
+        if (id_status[parseInt(this_id)]) {
+          $this.addClass(id_status[this_id])
+            .removeClass('get-status');
+        }
+      });
       
       $('.roadmap .get-status').removeClass('get-status'); //clear remaining throbbers
     };
+    
+    //add Done text to closed issues
+    var display_closed = function(data) {
+      $('a.get-status').each(function() {
+        var $this = $(this);
+        var id = $this.attr('id').split('-')[2];
+        
+        if ($.inArray(id, data)) {
+          $this.addClass('closed')
+            .removeClass('get-status');
+        }
+      });
+    }
     
     //use preloaded statuses or fetch and then display them
     if (settings.roadmap_statuses) {
       display_statuses(settings.roadmap_statuses);
     } else {
       //add throbber
-      $.getJSON(settings.ajax_path, '', display_statuses);
+      $.getJSON(settings.ajax_path + '/statuses', '', display_statuses);
+    }
+    
+    //as above, but for closed issues.
+    if (settings.roadmap_closed) {
+      display_closed(settings.roadmap_closed);
+    } else {
+      //add throbber
+      $.getJSON(settings.ajax_path + '/closed/' + settings.milestone, '', display_closed);
     }
   }
 };
